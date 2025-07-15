6149c2f8d10ee35849ab00ff6c70d1b678949a35
RayVA6 <rayvargas123666@gmail.com>
2025-07-15 10:26:35 -0500

    Update macetas.py

--- macetas.py
+++ macetas.py
@@ -23,4 +23,13 @@ if uploaded_file is not None:
     except Exception as e:
         st.error(f"Error al procesar la imagen: {e}")
 
+!pip install ultralytics
+from ultralytics import solutions
 
+inf = solutions.Inference(
+    model="yolo11n.pt",  # you can use any model that Ultralytics support, i.e. YOLO11, or custom trained model
+)
+
+inf.inference()
+
+# Make sure to run the file using command `streamlit run path/to/file.py`
